10fe09489f92c5f8163b431fc7b06119b1f6ee57
MaxRod30 <max.edw.rod@gmail.com>
2023-03-12 17:01:16 -0300

    Trabajo de navegacion

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Counter from './components/Counter/Counter';
+import {useState} from 'react'
 
 function App() {
+  const [view , setView] = useState('manzanas')
+
   return (
     <div className="App">
+      <div>
+        <button onClick={()=> setView('manzanas')}>Manzanas</button>
+        <button onClick={()=> setView('naranjas')}>Naranja</button>
+        <button onClick={()=> setView('bananas')}>Bananas</button>
+
+      </div>
         {/* <Navbar/> */}
-        <Counter />
+        {view =='manzanas' && <Counter title='Manzanas' min='5' max='10' />}
+        {view =='naranjas' &&<Counter title='Naranja' min='1' max='20' />}
+        {view =='bananas' &&<Counter title='Banana' min='1' max='20' />}
     </div>
   );
 }
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,24 +1,37 @@
 import {useState} from 'react'
 
-const Counter = () => {
+const Counter = ( {title , min , max}) => {
      
     const [count , setCount] = useState(0)
-    
+    const[msg , setMessage]=useState("")
+
     const increment = () =>{
-        for (let index = 0; index < 5; index++) {
-            setCount( prev =>prev + 1)
+        if(count < max){ 
+            setMessage("")
+            setCount( count + 1)
         }
-       
+        else{
+            setMessage(`No puede comprar max de ${max} ${title}`)
+        }
     }
     const decrement = () =>{
-        setCount( prev =>prev - 1)
+        if(count > min){ 
+            setMessage("")
+            setCount( count - 1) 
+        }
+        else{
+            setMessage(`No puede comprar menos de ${min} ${title}`)
+        }
     }
 
     return (
     <div>
+        <h1>{title}</h1>
+        <br></br>
         <h1>{count}</h1>
         <button onClick={decrement}>decrementar</button> 
         <button onClick={increment}>incrementar</button>
+        {msg !== '' && <p>{msg}</p>}
     </div>
     )
 
